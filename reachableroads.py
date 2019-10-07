be9802bfead19d565c425fc26883bd24b0fb93c5
Garfield Tong <garfield.tong@mines.sdsmt.edu>
2019-10-07 12:00:14 -0600

    finished reachableroads

--- reachableroads.py
+++ reachableroads.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 def dfs(node, neighborsOf, visited):
     if visited[node]:
         return
@@ -8,20 +7,31 @@ def dfs(node, neighborsOf, visited):
     for neighbor in neighborsOf[node]:
         dfs(neighbor, neighborsOf, visited)
 
+def bfs(node, neighborsOf, visited):
+    queue = []
+    queue.append(node)
+    visited[node] = True
+    while queue:
+        node = queue.pop(0)
+        for neighbor in neighborsOf[node]:
+            if not visited[neighbor]:
+                queue.append(neighbor)
+                visited[neighbor] = True
+
 
 for _ in range(int(input())):
     m = int(input())
     grid = [[] for j in range(m)]
-    count = 0
+    count = -1
     for i in range(int(input())):
         x, y = map(int, input().split())
         grid[x].append(y)
         grid[y].append(x)
-        visited = [False for j in range(m)]
-        dfs(0, grid, visited)
-        for i in range(m):
-            if visited:
-                continue
-            count += 1
-            dfs(i, grid, visited)
+        
+    visited = [False for j in range(m)]
+    for i in range(m):
+        if visited[i]:
+            continue
+        count += 1
+        dfs(i, grid, visited)
     print(count)
