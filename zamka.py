ed668572fcbef79741e099a766b0f71a4572a03d
Weilun Tong <wtong@c-lockinc.com>
2019-10-18 10:32:17 -0600

    finished zamka

---
+++ zamka.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def digitSum(n):
+    r = 0
+    while n:
+        r, n = r + n % 10, n // 10
+    return r
+
+
+L, D, X = map(int, [input() for i in range(3)])
+numList = []
+for i in range(L, D+1):
+    if digitSum(i) == X:
+        numList.append(i)
+
+x = numList.pop(0)
+y = numList.pop() if numList else x
+print(x, y, sep='\n')
